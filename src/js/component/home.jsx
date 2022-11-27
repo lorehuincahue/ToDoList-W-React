a41204d5d462309685b010ec0dc1cec08e55daf3
lorehuincahue <lorehuincahue@gmail.com>
2022-11-27 23:39:18 +0000

    ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+const [lista, setLista] = useState([])
+const norefresh =(evento) => {evento.preventDefault();
+console.log(evento.target[0].value)
+setLista([...lista,evento.target[0].value])
+}
+const DeleteItems = (indexItem) => {
+    setLista((prevState) =>
+      prevState.filter((elemento, indice) => indice !== indexItem)
+    );
+  };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div  className="container">
+			<form type="submit" onSubmit={norefresh}>
+			<h1 className="Titulo">Lista de pendientes</h1>
+			<input type="text"></input>
+			</form>
+			<ul>
+				{lista.map((elemento, indice)=>{
+					return <li key={indice}>{elemento} <button className="btn" onClick={() => DeleteItems(indice)}>
+					<i className="fas fa-trash-alt" />
+				  </button></li>
+				})}
+			</ul>
 		</div>
+
 	);
 };
-
 export default Home;
